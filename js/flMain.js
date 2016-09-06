dcf752678bdf77c5ffa4e9e7d5d3381070b16373
Kurt <kujo4pm@gmail.com>
2016-09-06 14:50:44 +1000

    Fixed

--- js/flMain.js
+++ js/flMain.js
@@ -810,24 +810,3 @@ function buildFilename(res)
        		}, 20);  
 
        }
-	       function fullScreen()
-	       {
-	       	if (document.fullscreenEnabled || 
-	       		document.webkitFullscreenEnabled || 
-	       		document.mozFullScreenEnabled ||
-	       		document.msFullscreenEnabled
-	       		) {
-	       		var i = document.documentElement;
-	// go full-screen
-	if (i.requestFullscreen) {
-		i.requestFullscreen();
-	} else if (i.webkitRequestFullscreen) {
-		i.webkitRequestFullscreen();
-	} else if (i.mozRequestFullScreen) {
-		i.mozRequestFullScreen();
-	} else if (i.msRequestFullscreen) {
-		i.msRequestFullscreen();
-	}
-
-	}
-	}
